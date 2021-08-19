344b7ee8a4b1862596c2766ec455e8a4b9946fe0
GentleScreamer <rathchinmay730@gmail.com>
2021-08-19 18:40:49 +0300

    d

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ module.exports.meme = require('./src/functions/meme');
 module.exports.blurplify = require('./src/functions/blurpify');
 module.exports.quickclick = require('./src/functions/buttons');
 
-module.exports.set = require('./src/functions/set');
-module.exports.fetch = require('./src/functions/fetch');
+
--- src/functions/fetch.js
+++
@@ -1,5 +0,0 @@
-const db = require('quick.db');
-module.exports = async(key, value) => {
-    let data = await db.fetch(key);
-    return data;
-}
