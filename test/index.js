8839737b9fd2dcba96be2995c581fa66bcbc7a56
GentleScreamer <rathchinmay730@gmail.com>
2021-08-23 13:16:28 +0300

    d

--- test/index.js
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-const db = require('../src/classes/database');
-=======
-const db = require('../src/classes/database');
->>>>>>> 78e4175693ecd6ad871dffbaa535d023e850540e
-data.set('test', true);
